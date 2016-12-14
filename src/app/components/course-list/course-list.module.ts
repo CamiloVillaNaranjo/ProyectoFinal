5c4cffc23ffa925126df023dae7fd96839b71a88
cvillanaranjo <camilo.villa@yuxipacific.com>
2016-12-14 17:10:07 -0500

    Updated Struct again to comply with best practices

--- src/app/components/course-list/course-list.module.ts
+++ src/app/components/course-list/course-list.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseListComponent } from './course-list.component';
-import { CoursedetailsComponent } from './coursedetails/coursedetails.component';
+import { CoursedetailsComponent } from './../coursedetails/coursedetails.component';
 
 @NgModule({
   imports: [
--- src/app/components/teachers-list/teachers-list.module.ts
+++ src/app/components/teachers-list/teachers-list.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeachersListComponent } from './teachers-list.component';
-import { TeacherdetailsComponent } from './teacherdetails/teacherdetails.component';
+import { TeacherdetailsComponent } from './../teacherdetails/teacherdetails.component';
 
 @NgModule({
   imports: [
